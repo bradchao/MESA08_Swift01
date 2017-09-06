21c651b67ff92b3070b522ad5d266e9e19b848ff
Brad <brad@brad.tw>
2017-09-06 10:47:51 +0800

    Initial Commit

---
+++ MySwift01/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  MySwift01
+//
+//  Created by user22 on 2017/9/6.
+//  Copyright © 2017年 Brad Big Company. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+var a = Int.max
+print(a)
+var b = a - 1
+print(b)
+
+
+
+
+
